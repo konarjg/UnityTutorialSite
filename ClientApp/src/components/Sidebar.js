e80f6923af465f6729813e670f37471112e8b3e4
Krzysztof <konarskikrzysztof1@gmail.com>
2025-01-19 15:16:12 +0100

    Some progress on tutorial sit

---
+++ ClientApp/src/components/Sidebar.js
@@ -0,0 +1,10 @@
+﻿import { React } from 'react';
+import './Sidebar.css';
+
+export function Sidebar() {
+    return (
+        <div className='container'>
+            
+        </div>
+    );
+}
--- ClientApp/src/components/Tutorials.js
+++ ClientApp/src/components/Tutorials.js
@@ -1,11 +1,40 @@
 ﻿import { React } from 'react';
+import { useState } from 'react';
 import { TopNav } from './TopNav';
+import { Sidebar } from './Sidebar';
 import './Tutorials.css';
 
 export function Tutorials() {
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+
+    function toggleSidebar() {
+        setSidebarVisible(!sidebarVisible);
+        var button = document.getElementById('sidebarButton');
+
+        if (sidebarVisible) {
+            button.style.width = "20vw";
+        }
+        else {
+            button.style.width = "2.75vw";
+        }
+    }
+
     return(
         <div className="main">
             <TopNav />
+            <div className="subTopbar">
+                <div className="sidebarButton" id="sidebarButton">
+                    <i className='fa fa-fw fa-bars' onClick={toggleSidebar} />
+                    <span id='sidebarTitle'>Tutorials</span>
+                </div>
+            </div>
+
+            <div className="content">
+                <Sidebar />
+                <div className="tutorial" id="tutorial">
+
+                </div>
+            </div>
         </div>
     );
 }
