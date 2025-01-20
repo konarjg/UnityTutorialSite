5c108fb1ae311a8916736851826528bc5e4d1d1d
Krzysztof <konarskikrzysztof1@gmail.com>
2025-01-20 01:03:49 +0100

    Sidebar design complete

--- ClientApp/src/components/Sidebar.js
+++ ClientApp/src/components/Sidebar.js
@@ -1,10 +1,48 @@
 ﻿import { React } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './Sidebar.css';
 
 export function Sidebar() {
+    const [categories, setCategories] = useState([]);
+
+    function toggleMenu(id) {
+        var element = document.getElementById(id);
+        var content = element.parentElement.parentElement.children[1];
+
+        var collapsed = element.classList.contains("fa-chevron-down");
+
+        if (collapsed) {
+            element.classList.remove("fa-chevron-down");
+            element.classList.add("fa-chevron-up");
+            content.style.height = "30vh";
+            content.style.visibility = "visible";
+        }
+        else {
+            element.classList.remove("fa-chevron-up");
+            element.classList.add("fa-chevron-down");
+            content.style.height = "0vh";
+            content.style.visibility = "collapse";
+        }
+    }
+
     return (
-        <div className='container'>
-            
+        <div className='sidebar' id='sidebar'>
+            {
+                categories.map(x =>
+                    <div className="mainCategoryContainer">
+                        <div className="mainCategoryTitle">
+                            <span className="mainCategory">Programming</span>
+                            <i id={'mainCategory' + x.categoryId} className='fa fa-fw fa-chevron-down expand' onClick={(event) => toggleMenu(event.currentTarget.id)} />
+                        </div>
+
+                        <div id={'mainCategoryContent' + x.categoryId} className="mainCategoryContent">
+
+                        </div>
+                    </div>
+                )
+            }
+           
         </div>
     );
 }
--- ClientApp/src/components/TopNav.js
+++ ClientApp/src/components/TopNav.js
@@ -2,16 +2,23 @@
 import { useNavigate } from 'react-router-dom';
 import './TopNav.css';
 
-export function TopNav() {
+export function TopNav({ route, onClick }) {
     const navigate = useNavigate();
 
     return(
         <div className="topnav">
-            <div className="title">Unity Tutorials</div>
+            {
+                route === '/Tutorials' &&
+                <button onClick={onClick} className="sidebarButton" id="sidebarButton">
+                    <i className='fa fa-fw fa-bars' />
+                </button>
+            }
+            
+            <button className="title" onClick={() => navigate('/')}>Unity Tutorials</button>
             <div className="buttons">
-                <button className="navButton" onClick={() => navigate('/')}>Home</button>
                 <button className="navButton" onClick={() => navigate('/Tutorials')}>Tutorials</button>
                 <button className="navButton" onClick={() => navigate('/About')}>About me</button>
+                <button className="navButton" onClick={() => navigate('/Login')}><i className="fa fa-fw fa-user-circle" /> Login</button>
             </div>
         </div>
     );
